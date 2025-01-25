21b23e46240d00fab56766593d76512923d16cf7
Belly <suchiha025@gmail.com>
2025-01-25 13:41:48 +0200

    reusable product item

---
+++ src/components/eCommerce/CartItem/CartItem.tsx
@@ -0,0 +1,68 @@
+import { memo } from "react";
+import { Button, Form } from "react-bootstrap";
+import { TProduct } from "@cutomTypes/product";
+
+import styles from "./styles.module.css";
+const { cartItem, product, productImg, productInfo, cartItemSelection } =
+  styles;
+
+type TCartItem = TProduct & {
+  changeQuantityHandler: (id: number, quantity: number) => void;
+  removeItemHandler: (id: number) => void;
+};
+
+const CartItem = memo(
+  ({
+    id,
+    title,
+    price,
+    image,
+    max,
+    quantity,
+    changeQuantityHandler,
+    removeItemHandler,
+  }: TCartItem) => {
+    const renderOptions = Array(max)
+      .fill(0)
+      .map((_, idx) => {
+        const quantity = ++idx;
+        return <option value={quantity}>{quantity}</option>;
+      });
+
+    const changeQuantity = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      const quantity = +event.target.value;
+      changeQuantityHandler(id, quantity);
+    };
+
+    return (
+      <div className={cartItem}>
+        <div className={product}>
+          <div className={productImg}>
+            <img src={image} alt={title} />
+          </div>
+          <div className={productInfo}>
+            <h2>{title}</h2>
+            <h3>{price} EGP</h3>
+            <Button
+              variant="secondary"
+              style={{ color: "white" }}
+              className="mt-auto"
+              onClick={() => removeItemHandler(id)}
+            >
+              Remove
+            </Button>
+          </div>
+        </div>
+
+        <div className={cartItemSelection}>
+          <span className="d-block mb-1">Quantity</span>
+          <Form.Select value={quantity} onChange={changeQuantity}>
+            {renderOptions}
+          </Form.Select>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default CartItem;

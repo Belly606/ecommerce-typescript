264864d7c61f1e65d2366846209f0265841d1f58
Belly <suchiha025@gmail.com>
2025-01-25 13:43:37 +0200

    change quantity, remove item & get products actions

--- src/store/cart/cartSlice.ts
+++ src/store/cart/cartSlice.ts
@@ -1,15 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+import actGetProductsByItemsId from "./act/actGetProductsByItemsId";
 import { getCartTotalQuantitySelector } from "./selectors";
 import { TProduct } from "@cutomTypes/product";
+import { TLoading } from "@cutomTypes/shared";
 
 type TCartState = {
-  items: { [key: number]: number };
-  productFullInfo: TProduct[];
+  items: { [key: string]: number };
+  productsFullInfo: TProduct[];
+  loading: TLoading;
+  error: null | string;
 };
 
 const initialState: TCartState = {
   items: {},
-  productFullInfo: [],
+  productsFullInfo: [],
+  loading: "idle",
+  error: null,
 };
 
 const cartSlice = createSlice({
@@ -24,9 +30,35 @@ const cartSlice = createSlice({
         state.items[id] = 1;
       }
     },
+    cartItemChangeQuantity: (state, action) => {
+      state.items[action.payload.id] = action.payload.quantity;
+    },
+    cartItemRemove: (state, action) => {
+      delete state.items[action.payload];
+      state.productsFullInfo = state.productsFullInfo.filter(
+        (el) => el.id !== action.payload
+      );
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(actGetProductsByItemsId.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    });
+    builder.addCase(actGetProductsByItemsId.fulfilled, (state, action) => {
+      state.loading = "succeeded";
+      state.productsFullInfo = action.payload;
+    });
+    builder.addCase(actGetProductsByItemsId.rejected, (state, action) => {
+      state.loading = "failed";
+      if (action.payload && typeof action.payload === "string") {
+        state.error = action.payload;
+      }
+    });
   },
 });
 
-export { getCartTotalQuantitySelector };
-export const { addToCart } = cartSlice.actions;
+export { getCartTotalQuantitySelector, actGetProductsByItemsId };
+export const { addToCart, cartItemChangeQuantity, cartItemRemove } =
+  cartSlice.actions;
 export default cartSlice.reducer;

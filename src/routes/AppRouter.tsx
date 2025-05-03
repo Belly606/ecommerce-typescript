c93e14af79981d870502ce1d99c805d16b5b9906
Belly <suchiha025@gmail.com>
2025-05-03 22:41:17 +0300

    feat: Seperate routing code

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,50 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// Layouts
+import { MainLayout } from "@layouts/index";
+
+// Pages
+import Home from "@pages/Home";
+import AboutUs from "@pages/AboutUs";
+import Categories from "@pages/Categories";
+import Product from "@pages/Products";
+import Login from "@pages/Login";
+import Register from "@pages/Register";
+import Error from "@pages/Error";
+
+const AppRouter = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      errorElement: <Error />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/about-us", element: <AboutUs /> },
+        { path: "/categories", element: <Categories /> },
+        {
+          path: "/products/:prefix",
+          element: <Product />,
+          loader: ({ params }) => {
+            if (
+              typeof params.prefix !== "string" ||
+              !/^[a-z]+$/i.test(params.prefix)
+            ) {
+              throw new Response("Bad request", {
+                status: 400,
+                statusText: "Category not Found",
+              });
+            }
+            return true;
+          },
+        },
+        { path: "/register", element: <Register /> },
+        { path: "/login", element: <Login /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default AppRouter;

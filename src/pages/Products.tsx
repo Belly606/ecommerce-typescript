a9932f126d197cc0c7c32b890627a9f84f6b4c50
Belly <suchiha025@gmail.com>
2025-05-04 10:37:53 +0300

    build: load dynamic data from db

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,26 +1,43 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useAppSelector, useAppDispatch } from "@store/hooks";
+import {
+  actGetProductsByCatPrefix,
+  productsCleanUp,
+} from "@store/products/productsSlice";
 import { Product } from "@components/eCommerce";
 import { Container, Row, Col } from "react-bootstrap";
 
 const Products = () => {
+  const params = useParams();
+  const dispatch = useAppDispatch();
+  const { loading, error, records } = useAppSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(actGetProductsByCatPrefix(params.prefix as string));
+
+    return () => {
+      dispatch(productsCleanUp());
+    };
+  }, [dispatch, params]);
+
+  const ProductsList =
+    records.length > 0
+      ? records.map((record) => (
+          <Col
+            xs={6}
+            md={3}
+            key={record.id}
+            className="d-flex justify-content-center mb-5 mt-2"
+          >
+            <Product {...record} />
+          </Col>
+        ))
+      : "There is no products";
+
   return (
     <Container>
-      <Row>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-      </Row>
+      <Row>{ProductsList}</Row>
     </Container>
   );
 };

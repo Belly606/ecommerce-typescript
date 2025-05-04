95c2f82504de6e1c7580fe39f86e9e518c58e239
Belly <suchiha025@gmail.com>
2025-05-04 10:43:53 +0300

    creating products redux store & actions

---
+++ src/store/products/productsSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+import actGetProductsByCatPrefix from "./act/actGetProductsByCatPrefix";
+import { TLoading } from "@customTypes/shared";
+import { TProduct } from "@customTypes/product";
+
+type TProductssState = {
+  records: TProduct[];
+  loading: TLoading;
+  error: string | null;
+};
+
+const initialState: TProductssState = {
+  records: [],
+  loading: "idle",
+  error: null,
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    productsCleanUp: (state) => {
+      state.records = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(actGetProductsByCatPrefix.fulfilled, (state, action) => {
+      state.loading = "idle";
+      state.records = action.payload;
+    });
+    builder.addCase(actGetProductsByCatPrefix.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    });
+    builder.addCase(actGetProductsByCatPrefix.rejected, (state, action) => {
+      state.loading = "failed";
+      if (action.payload && typeof action.payload === "string") {
+        state.error = action.payload;
+      }
+    });
+  },
+});
+
+export const { productsCleanUp } = productsSlice.actions;
+export { actGetProductsByCatPrefix };
+export default productsSlice.reducer;

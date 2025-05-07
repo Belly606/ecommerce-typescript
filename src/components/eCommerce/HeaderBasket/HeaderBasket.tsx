79b5be1d0811a501b53f00c7b33ab3f953f6f148
Belly <suchiha025@gmail.com>
2025-05-07 03:56:59 +0300

    build: adding a dynamic items counter on cart and animate it

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react";
+import { useAppSelector } from "@store/hooks";
+import { getCartTotalQuantitySelector } from "@store/cart/cartSlice";
 import Logo from "@assets/svg/cart.svg?react";
 
 import styles from "./styles.module.css";
-const { basketContainer, basketQuantity } = styles;
+const { basketContainer, basketQuantity, pumpCartQuantity, basketCart } =
+  styles;
 
 const HeaderBasket = () => {
+  const [isAnimate, setIsAnimate] = useState(false);
+  const totalQuantity = useAppSelector(getCartTotalQuantitySelector);
+  const quantityStyle = isAnimate
+    ? `${basketQuantity} ${pumpCartQuantity}`
+    : basketQuantity;
+
+  useEffect(() => {
+    if (!totalQuantity) {
+      return;
+    }
+
+    setIsAnimate(true);
+
+    const debounce = setTimeout(() => {
+      setIsAnimate(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [totalQuantity]);
   return (
     <div className={basketContainer}>
-      <Logo title="Basket Icon" />
-      <div className={basketQuantity}>0</div>
+      <div className={basketCart}>
+        <Logo title="Basket Icon" />
+        <div className={quantityStyle}>{totalQuantity}</div>
+      </div>
+      <h3>Cart</h3>
     </div>
   );
 };

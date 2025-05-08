251cd1132e7e5b9917a256358b1d7421e725061e
Belly <suchiha025@gmail.com>
2025-05-08 14:52:51 +0300

    build: render wishlist

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,9 +1,34 @@
-import { Heading } from "@components/common";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { actGetWishlist } from "@store/wishlist/wishlistSlice";
+import { GridList, Heading } from "@components/common";
+import { Product } from "@components/eCommerce";
+import { Loading } from "@components/feedback";
 
 const Wishlist = () => {
+  const dispatch = useAppDispatch();
+  const { loading, error, productsFullInfo } = useAppSelector(
+    (state) => state.wishlist
+  );
+  const cartItems = useAppSelector((state) => state.cart.items);
+
+  const records = productsFullInfo.map((el) => ({
+    ...el,
+    quantity: cartItems[el.id] || 0,
+    isLiked: true,
+  }));
+  useEffect(() => {
+    dispatch(actGetWishlist());
+  }, [dispatch]);
   return (
     <>
       <Heading>Your Wishlist</Heading>
+      <Loading status={loading} error={error}>
+        <GridList
+          records={records}
+          renderItem={(record) => <Product {...record} />}
+        />
+      </Loading>
     </>
   );
 };

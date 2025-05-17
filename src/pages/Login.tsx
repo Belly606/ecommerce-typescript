ebb60d399d77f3f48a7b9338cfb550a77c2bd90e
Belly <suchiha025@gmail.com>
2025-05-17 07:30:26 +0300

    build: login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,34 @@
+import { Heading } from "@components/common";
+import { Form, Button, Row, Col } from "react-bootstrap";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Heading title="Login" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="text" name="email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" name="password" />
+            </Form.Group>
+
+            <Button variant="info" type="submit" style={{ color: "white" }}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Login;
